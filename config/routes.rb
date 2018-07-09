e51160db54eca023065f412b9ccb05739c55a898
tomo <arm_93_0618@yahoo.co.jp>
2018-07-09 09:03:57 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get  'signup', to:'users#new'
+  resources :users, only:[:show, :create]
+  
+  get 'sessions/create'
+  get 'sessions/destroy'
+
   root to: 'tasks#index'
+  
+  get 'login', to: 'tasks#index'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :tasks
 end
